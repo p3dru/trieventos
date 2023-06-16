430aa75e9af30f6479bc8551c9fa9b3c318df2fa
João Pedro <61830482+p3dru@users.noreply.github.com>
2023-06-16 09:31:50 -0300

    Add files via upload

--- trieventos-v2.0/app.js
+++ trieventos-v2.0/app.js
@@ -1,80 +1,88 @@
 const express = require("express");
 const ejs = require('ejs');
-
+const path = require('path');
 
 
 const app = express();
 
 app.set('view engine', 'ejs');
 
+
+//para servir arquivos estáticos ao CSS
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.get('/', function(req, res){
     //console.log(res.json);
-    res.render('home');
+    res.render('home', {titleTag: 'Home'});
 });
 
 app.get('/sobre', function(req, res){
-    res.render('sobre');
+    res.render('sobre', {titleTag: 'Sobre'});
 });
 
 app.get('/sobre/planos', function(req, res){
-    res.render('planos');
+    res.render('planos', {titleTag: 'Planos'});
 });
 
 app.get('/estabelecimentos/x', function(req,res){
-    res.render('estabelecimentos');
+    res.render('estabelecimentos', {titleTag: 'Estabelecimentos'});
 });
 
 app.get('/estabelecimentos/x/horarios', function(req,res){
-    res.render('horarios');
+    const horas = ['17:00', '18:00', '19:00', '20:00', '21:00', '22:00', '23:00', '00:00'];
+    const diasSemana = ['Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado'];
+    const ocupados = [{dia: 'Segunda', hora:'18:00'}, {dia: 'Sexta', hora: '22:00'},
+                    {dia: 'Sábado', hora: '21:00'}, {dia: 'Sábado', hora: '20:00'}];
+    res.render('horarios', { horas, diasSemana, titleTag: 'Horários', ocupados});
 });
 
 //gets-logins
 //login padrão
 app.get('/login', function(req, res){
-    res.render('login/login');
+    res.render('login/inicial', {titleTag: 'Login'});
 });
 
 //login de usuário comum
 app.get('/login-locatarios', function(req, res){
-    res.render('login/login-usuarios');
+    res.render('login/login-usuarios', {titleTag: 'Login'});
 })
 
 //login de estabelecimentos
 app.get('/login-estabelecimentos', function(req, res){
-    res.render('login/login-estabelecimentos')
+    res.render('login/login-estabelecimentos', {titleTag: 'Estabelecimentos'})
 })
 
 app.get('/perfil', function(req, res){
-    res.render('perfil');
+    res.render('perfil', {titleTag: 'Perfil'});
 });
 
 app.get('/cadastrar-usuario', function(req, res){
-    res.render('cadastro-usuario');
+    res.render('cadastro-usuario', {titleTag: 'Cadastrar Usuário'});
 });
 
 app.get('/redefinir-senha', function(req, res){
-    res.render('redefinir-senha');
+    res.render('redefinir-senha', {titleTag: 'Redefinir Senha'});
 })
 
 app.get('/x/gerenciar', function(req, res){
-    res.render('alterar-perfil');
+    res.render('alterar-perfil', {titleTag: 'Alterar Perfil'});
 })
 
 app.get('/x/locacoes', function(req, res){
-    res.render('locacoes');
+    res.render('locacoes', {titleTag: 'Histórico de Locações'});
 })
 
 
 
 //gets de estabelecimentos
 app.get('/login-estabelecimentos', function(req, res){
-    res.render('login-estabelecimentos');
+    res.render('login-estabelecimentos', {titleTag: 'Login'});
 })
 
 //tela inicial dos estabelecimentos
 app.get('/local', function(req, res){
     //trocar o local para pegar o usuário pelo parâmetro
-    res.render('estabelecimentos/home');
+    res.render('estabelecimentos/home', {titleTag: 'Home'});
 })
 
 //posts
--- trieventos-v2.0/testes/teste.js
+++ trieventos-v2.0/testes/teste.js
@@ -15,5 +15,26 @@ var links = [
 ]
 
 links.forEach((option) => {
-    console.log(option.key)
+    //console.log(option.key)
 })
+
+var comparativo = [{i: 6, j: 1}, {i: 1, j: 1}, {i: 2, j: 2}, {i: 3, j: 3}, {i: 4, j: 4}]
+//console.log(comparativo.length)
+
+for (let i = 0; i < 5; i++){
+    for (let j = 0; j < 5; j++){
+        for (let h = 0; h < comparativo.length; h++){
+            if (i == comparativo[h].i) {
+                if (j == comparativo[h].j){
+                    console.log("[xx]");
+                } else {
+                    if (j != comparativo[h].j){
+                        console.log('[diferente]');
+                    }
+                }
+            }
+        }
+    }
+    //console.log()
+}
+
